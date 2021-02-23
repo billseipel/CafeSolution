a4b8e559287ebb41ac2ddf2db219d0f1f6a48cfa
Bill Seipel <bill.seipel@unifac.com>
2021-02-23 11:03:59 -0600

    Added Unit Tests where order Total is not rounded up and Tests where order total IS rouneded up.

--- Cafe.Testing.NUnit/UnitTest1.cs
+++ Cafe.Testing.NUnit/UnitTest1.cs
@@ -41,5 +41,25 @@ namespace Cafe.Testing.NUnit
             Assert.That(order.OrderTotal, Is.EqualTo(d));
             
         }
+
+        [Test]
+        public void TotalIsRoundedToOddValues()
+        {
+            decimal d = 8.78M;
+
+            //create the order
+            OrderItem oi1 = new OrderItem(new MenuItem("Cola", 0.53M, false, false), 3);
+            OrderItem oi2 = new OrderItem(new MenuItem("Cheese Sandwich", 2.13M, true, false), 3);
+            Order order = new Order(new List<OrderItem>() { oi1, oi2 });
+
+            //create the Checkout service instance
+            CheckoutService checkoutService = new CheckoutService(order);
+            checkoutService.CalculateAll(order);
+
+            //order sub total should be 7.98
+            // service charge should be .80 (rounded from .798)
+            Assert.That(order.OrderTotal, Is.EqualTo(d));
+
+        }
     }
 }
